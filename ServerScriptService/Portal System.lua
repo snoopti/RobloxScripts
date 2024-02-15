c2025c812e0e989e1ef7a6d0edbfcba102c7a9dd
JONAS <92232204+snoopti@users.noreply.github.com>
2024-02-15 20:00:42 +0100

    Update Portal System.lua

--- ServerScriptService/Portal System.lua
+++ ServerScriptService/Portal System.lua
@@ -1,9 +1,9 @@
 -- This script allows you to create a portal system, you have 2 parts a start point and an end point, when you enter the start point(from) you will be teleported to the end point(to).
 
--- Create a folder[1] named "Warps" in the workspace.
+-- Create a folder[1] named "Portals" in the workspace.
 -- Create 2 parts, "fromX" and "toX", where X is the number of the portal.
 
-local folder = game.Workspace.Warps -- 1
+local folder = game.Workspace.Portals -- 1
 
 local function Warp(num: number, hit: BasePart)
 	local char = hit.Parent
